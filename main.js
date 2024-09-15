cb181bb739969565a4c2db5b6012a3d536097d84
burning-monk <18663097+burning-monk@users.noreply.github.com>
2024-09-15 22:13:47 +0200

    Fix: Issue with success message visibilitiy

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-var validate = new Bouncer(".signup__form", {
+var validate = new Bouncer("#signup-form", {
   messageAfterField: false,
 });
 
-const signupForm = document.querySelector(".signup__form");
+const signupForm = document.querySelector("#signup-form");
+const signup = document.querySelector(".signup");
+const successContainer = document.querySelector(".signup__success-container");
+const formContainer = document.querySelector(".signup__form-container");
 
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,20 +14,21 @@ signupForm.addEventListener("submit", (e) => {
   var invalidFields = validate.validateAll(signupForm);
 
   if (invalidFields.length === 0) {
-    const signup = document.querySelector(".signup");
     const input = document.querySelector("#email");
     const span = document.querySelector(".success-message__email-address");
     span.textContent = input.value;
+
+    // Add the success class and hide the form container
     signup.classList.add("signup--success");
+    successContainer.style.display = "flex";
+    formContainer.style.display = "none";
   }
 });
 
-const dismissBtn = document.querySelector(".success-message__dismiss-btn");
-
-dismissBtn.addEventListener("click", () => {
-  const signup = document.querySelector(".signup");
-
+// Handle dismiss button
+document.querySelector(".success-message__dismiss-btn").addEventListener("click", () => {
   signup.classList.remove("signup--success");
-
-  signupForm.reset();
+  signupForm.reset(); 
+  successContainer.style.display = "none";
+  formContainer.style.display = "flex"; 
 });
