f209ba17c835eaf13678f1d6b9fbe722883b45c6
burning-monk <18663097+burning-monk@users.noreply.github.com>
2024-09-15 20:50:36 +0200

    Ensure form fields are valid with Bouncer.js before triggering success state

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-
+var validate = new Bouncer(".signup__form", {
+  messageAfterField: false,
+});
 
 const signupForm = document.querySelector(".signup__form");
 
 signupForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // Run Bouncer validation manually
+  var invalidFields = validate.validateAll(signupForm);
+
+  if (invalidFields.length === 0) {
     const signup = document.querySelector(".signup");
     const input = document.querySelector("#email");
     const span = document.querySelector(".success-message__email-address");
-    console.log(span);
     span.textContent = input.value;
     signup.classList.add("signup--success");
+  }
 });
 
 const dismissBtn = document.querySelector(".success-message__dismiss-btn");
 
 dismissBtn.addEventListener("click", () => {
   const signup = document.querySelector(".signup");
+
   signup.classList.remove("signup--success");
-});
-var validate = new Bouncer(".signup__form", {
-  messageAfterField: false,
+
+  signupForm.reset();
 });
